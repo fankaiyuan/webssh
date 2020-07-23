94070cceda127602d481d7bcebde4b050277321a
fankaiyuan <172881969@qq.com>
2020-07-22 21:32:13 -0400

    update files

--- docker-build.sh
+++ docker-build.sh
@@ -15,7 +15,7 @@ arch=$2   # arm arm64 amd64
 if [[ -n $sha ]]; then
         tag=kaiyfan/webssh-tags:$arch
         echo $tag
-        sed "s#{{base_image}}#$base_image#g" Dockerfile.template > Dockerfile.$arch
+        sed "s|{{base_image}}|$base_image|g" Dockerfile.template > Dockerfile.$arch
         docker build -t $tag -f Dockerfile.$arch .
         docker push $tag
 fi
