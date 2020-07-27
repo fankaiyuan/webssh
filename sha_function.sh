5b67d72f94bc6765a036fd8a5e232e8573049a8a
fankaiyuan <172881969@qq.com>
2020-07-27 16:41:20 -0400

    refactor files

--- sha_function.sh
+++ sha_function.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 get_sha(){
-    repo=$1
     docker pull $1 &>/dev/null
-    #sha=$(docker image inspect $1 |jq .[0].RootFS.Layers |grep sha)
     sha=$(docker image inspect $1 | jq --raw-output '.[0].RootFS.Layers|.[]')   # [0] means first element of list,[]means all the elments of lists
     echo $sha
 }
@@ -11,27 +9,26 @@ get_sha(){
 is_base (){
     local base_sha    # alpine
     local image_sha   # new image
-    base_repo=$1
-    image_repo=$2
-    base_sha=$(get_sha $1)
-    image_sha=$(get_sha $2)
+    local base_repo=$1
+    local image_repo=$2
 
-    found="true"
+    base_sha=$(get_sha $base_repo)
+    image_sha=$(get_sha $image_repo)
+
     for i in $base_sha; do
         for j in $image_sha; do
             if [[ $i = $j ]]; then
-                #echo "no change, same base image: $i"
-                found="false"
+                true    #same base image
                 break
             fi
         done
     done
-    echo "$found"
+    false
 }
 
 get_service_version(){
     local version
-    repo=$1
+    local repo=$1
     docker run -d $repo &>/dev/null
     container_id=$(docker ps | grep "$repo" | awk '{print$1;}')
     version=$(docker exec -it $container_id wssh --version)
@@ -40,10 +37,9 @@ get_service_version(){
 }
 
 compare (){
-    result=$(is_base $1 $2)
     version1=$(get_service_version $3)
     version2=$(get_service_version $4)
-    if [ $result == "true" ] || [ "$version1" != "$version2" ]; #compare alpine and service versions
+    if [ !$(is_base $1 $2) ] || [ "$version1" != "$version2" ];     #compare alpine and service versions
     then
         echo "true"
     else
@@ -52,18 +48,19 @@ compare (){
 }
 
 create_manifest (){
-    local repo=$1 #kaiyfan/webssh
-    local tag_latest=$2 #latest
-    local tag_time=$3 #timetag
-    local tag_arm=$4  #kaiyfan/webssh-tags:arm
+    local repo=$1           #treehouses/webssh
+    local tag_latest=$2     #latest
+    local tag_time=$3       #timetag
+    local tag_arm=$4        #treehouses/webssh-tags:arm
     local tag_x86=$5
     local tag_arm64=$6
-    docker manifest create $repo:$tag_latest $tag_arm $tag_x86 $tag_arm64
-    docker manifest create $repo:$tag_time $tag_arm $tag_x86 $tag_arm64
-    docker manifest annotate $repo:$tag_latest $tag_arm --arch arm
-    docker manifest annotate $repo:$tag_latest $tag_x86 --arch amd64
+    docker manifest create   $repo:$tag_latest $tag_arm $tag_x86 $tag_arm64
+    docker manifest create   $repo:$tag_time   $tag_arm $tag_x86 $tag_arm64
+
+    docker manifest annotate $repo:$tag_latest $tag_arm   --arch arm
+    docker manifest annotate $repo:$tag_time   $tag_arm   --arch arm
+    docker manifest annotate $repo:$tag_latest $tag_x86   --arch amd64
+    docker manifest annotate $repo:$tag_time   $tag_x86   --arch amd64
     docker manifest annotate $repo:$tag_latest $tag_arm64 --arch arm64
-    docker manifest annotate $repo:$tag_time $tag_arm --arch arm
-    docker manifest annotate $repo:$tag_time $tag_x86 --arch amd64
-    docker manifest annotate $repo:$tag_time $tag_arm64 --arch arm64
+    docker manifest annotate $repo:$tag_time   $tag_arm64 --arch arm64
 }
